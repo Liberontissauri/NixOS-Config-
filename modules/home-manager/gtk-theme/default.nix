1374aa53ad27ca89592a98f700aa0413b3b8d599
Liberontissauri <jp.liberon@gmail.com>
2023-09-10 22:17:56 +0100

    theme changed

--- modules/home-manager/gtk-theme/default.nix
+++ modules/home-manager/gtk-theme/default.nix
@@ -2,9 +2,33 @@
 
   gtk = {
     enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
     theme = {
-      name = "Fluent";
-      package = pkgs.fluent-gtk-theme;
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
     };
   };
+
+  home.sessionVariables.GTK_THEME = "palenight";
 }
