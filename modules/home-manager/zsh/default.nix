f3e93b65ead5cd9c25e8695d3831f9322fd49577
Liberontissauri <jp.liberon@gmail.com>
2023-09-10 02:41:48 +0100

    added zsh support

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,8 +1,13 @@
 { pkgs, ...}: {
   programs.zsh.enable = true;
   programs.starship.enable = true;
+  programs.starship.enableZshIntegration = true;
 
   programs.zsh.initExtra = "
     pfetch
   ";
+
+  home.file.".config/starship.toml" = {
+    source = ./config/starship.toml;
+  };
 }
