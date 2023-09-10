6b8a7aa7827c3cfc8d8185e1e71ebf741752dfab
Liberontissauri <jp.liberon@gmail.com>
2023-09-10 21:52:27 +0100

    changed laptop config hostname to lil-lib

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     system = "x86_64-linux";
   in {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      lil-lib = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           ./modules/nixos/configuration.nix
