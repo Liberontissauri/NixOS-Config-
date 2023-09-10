e0d9003b4a5a9940b468784c43524edf1ea91238
Liberontissauri <jp.liberon@gmail.com>
2023-09-10 03:07:40 +0100

    added oh my zsh

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,9 +1,12 @@
 { pkgs, ...}: {
   programs.zsh.enable = true;
+  programs.zsh.oh-my-zsh.enable = true;
   programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
 
   programs.zsh.initExtra = "
+    bindkey \"^[[1;5C\" forward-word
+    bindkey \"^[[1;5D\" backward-word
     pfetch
   ";
 
